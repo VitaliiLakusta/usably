cc8c9a477b708a51fff7130a43445a78731afbff
Vitalii Lakusta <lakusta96@gmail.com>
2015-04-23 03:08:50 +0300

    added three 'boxes' into main page: rate, history chart, critical issues

--- usably-laravel/resources/views/index.blade.php
+++ usably-laravel/resources/views/index.blade.php
@@ -1,273 +1,95 @@
-@extends('app');
+
+@extends('app')
+
+@section('header')
+    <script type="text/javascript" src="/js/rating_star.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            //tooltip
+            $('[data-toggle="tooltip"]').tooltip(),
+                //add to list animation
+                    $('.add-to-list').click(function AddIssueToList(){
+                        $(this).parent().parent('.issue').stop(true,true).animate({marginLeft:'150px',opacity:'hide'},700);
+                    }),
+                //success alert animation
+                    $(".add-to-list").click(function showSuccessAlert() {
+                        setTimeout(function() {
+                            $(".alert-success").alert().slideDown(700);
+                            $(".alert-success").fadeTo(1500, 1).slideUp(700, function(){
+                                $(".alert-success").hide();
+                            });
+                        },300)
+                    }),
+                //error alert animation
+                    $(".error").click(function showErrorAlert() {
+                        setTimeout(function() {
+                            $(".alert-danger").alert().slideDown(700);
+                            $(".alert-danger").fadeTo(1500, 1).slideUp(700, function(){
+                                $(".alert-danger").hide();
+                            });
+                        },300)
+                    });
+        });
+
+    </script>
+@stop
 
 @section('content')
-
-    <!-- Page Heading -->
     <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">
-                Dashboard <small>Statistics Overview</small>
-            </h1>
+        <div class="col-md-12">
+            <h1>Dashboard <small>Usability Overview</small></h1>
+            <hr>
             <ol class="breadcrumb">
-                <li class="active">
-                    <i class="fa fa-dashboard"></i> Dashboard
-                </li>
+                <li class="active"><i class="fa fa-dashboard"></i><a href="#">Dashboard</a></li>
             </ol>
         </div>
     </div>
-    <!-- /.row -->
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="alert alert-info alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <i class="fa fa-info-circle"></i>  <strong>Like SB Admin?</strong> Try out <a href="http://startbootstrap.com/template-overviews/sb-admin-2" class="alert-link">SB Admin 2</a> for additional features!
+    <div class="row indicator-panel">
+        
+        <div class="col-md-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading" style="padding-bottom:15px;">
+                    <div class="col-md-6 usably_rate_star">
+                        <canvas id="star_rate" width="70" height="70"></canvas>
+                    </div>
+                    <div class="col-md-6 usably_rate_info">
+                        <span id="counter" data-fill="60"></span><span style="font-size:70%;color:#0F426E">/100</span>
+                        <span style="font-size:50%;font-weight:bold;display:block;margin-left:-25px">Usability Rate</span>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <div class="view-usably-details"><a href="#">View details<i class="fa fa-info-circle"></i></a></div>
+                </div>
             </div>
         </div>
-    </div>
-    <!-- /.row -->
 
-    <div class="row">
-        <div class="col-lg-3 col-md-6">
+        <div class="col-md-6">
             <div class="panel panel-primary">
                 <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-comments fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">26</div>
-                            <div>New Comments!</div>
-                        </div>
-                    </div>
+                    <span style="font-size:18px">Usability rate over time</span>
                 </div>
-                <a href="#">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Details</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
+                <div class="panel-body">
+                    <canvas id="lineChart1" height="50"></canvas>
+                </div>
             </div>
         </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-green">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-tasks fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">12</div>
-                            <div>New Tasks!</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="#">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Details</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-yellow">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-shopping-cart fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">124</div>
-                            <div>New Orders!</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="#">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Details</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-red">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-support fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">13</div>
-                            <div>Support Tickets!</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="#">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Details</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-    <!-- /.row -->
 
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title"><i class="fa fa-bar-chart-o fa-fw"></i> Area Chart</h3>
-                </div>
-                <div class="panel-body">
-                    <div id="morris-area-chart"></div>
-                </div>
-            </div>
+        <div class="col-md-3">
+            @include('issues.critical-issues')
         </div>
-    </div>
-    <!-- /.row -->
 
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title"><i class="fa fa-long-arrow-right fa-fw"></i> Donut Chart</h3>
-                </div>
-                <div class="panel-body">
-                    <div id="morris-donut-chart"></div>
-                    <div class="text-right">
-                        <a href="#">View Details <i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title"><i class="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="list-group">
-                        <a href="#" class="list-group-item">
-                            <span class="badge">just now</span>
-                            <i class="fa fa-fw fa-calendar"></i> Calendar updated
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">4 minutes ago</span>
-                            <i class="fa fa-fw fa-comment"></i> Commented on a post
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">23 minutes ago</span>
-                            <i class="fa fa-fw fa-truck"></i> Order 392 shipped
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">46 minutes ago</span>
-                            <i class="fa fa-fw fa-money"></i> Invoice 653 has been paid
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">1 hour ago</span>
-                            <i class="fa fa-fw fa-user"></i> A new user has been added
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">2 hours ago</span>
-                            <i class="fa fa-fw fa-check"></i> Completed task: "pick up dry cleaning"
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">yesterday</span>
-                            <i class="fa fa-fw fa-globe"></i> Saved the world
-                        </a>
-                        <a href="#" class="list-group-item">
-                            <span class="badge">two days ago</span>
-                            <i class="fa fa-fw fa-check"></i> Completed task: "fix error on sales page"
-                        </a>
-                    </div>
-                    <div class="text-right">
-                        <a href="#">View All Activity <i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title"><i class="fa fa-money fa-fw"></i> Transactions Panel</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-hover table-striped">
-                            <thead>
-                            <tr>
-                                <th>Order #</th>
-                                <th>Order Date</th>
-                                <th>Order Time</th>
-                                <th>Amount (USD)</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>3326</td>
-                                <td>10/21/2013</td>
-                                <td>3:29 PM</td>
-                                <td>$321.33</td>
-                            </tr>
-                            <tr>
-                                <td>3325</td>
-                                <td>10/21/2013</td>
-                                <td>3:20 PM</td>
-                                <td>$234.34</td>
-                            </tr>
-                            <tr>
-                                <td>3324</td>
-                                <td>10/21/2013</td>
-                                <td>3:03 PM</td>
-                                <td>$724.17</td>
-                            </tr>
-                            <tr>
-                                <td>3323</td>
-                                <td>10/21/2013</td>
-                                <td>3:00 PM</td>
-                                <td>$23.71</td>
-                            </tr>
-                            <tr>
-                                <td>3322</td>
-                                <td>10/21/2013</td>
-                                <td>2:49 PM</td>
-                                <td>$8345.23</td>
-                            </tr>
-                            <tr>
-                                <td>3321</td>
-                                <td>10/21/2013</td>
-                                <td>2:23 PM</td>
-                                <td>$245.12</td>
-                            </tr>
-                            <tr>
-                                <td>3320</td>
-                                <td>10/21/2013</td>
-                                <td>2:15 PM</td>
-                                <td>$5663.54</td>
-                            </tr>
-                            <tr>
-                                <td>3319</td>
-                                <td>10/21/2013</td>
-                                <td>2:13 PM</td>
-                                <td>$943.45</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="text-right">
-                        <a href="#">View All Transactions <i class="fa fa-arrow-circle-right"></i></a>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
-    <!-- /.row -->
+
+
+
+
+@stop
+
+@section('footer')
+    <script src="/js/Chart.js"></script>
+    <script src="/js/main.js"></script>
+
+@stop
 
 @stop
---
+++ usably-laravel/resources/views/issues/critical-issues.blade.php
@@ -0,0 +1,75 @@
+<div class="alert alert-success" style="display:none;">
+    <a href="#" class="close" data-dismiss="alert">&times;</a>
+    <strong>Success!</strong> Issue has been added to list succesfully.
+</div>
+
+<div class="alert alert-danger" style="display:none;">
+    <a href="#" class="close" data-dismiss="alert">&times;</a>
+    <strong>Error!</strong> An error occured while adding issue to list. This information is just for demonstrate.
+</div>
+
+<div class="panel panel-danger">
+    <div class="panel-heading issue-heading">Critical Issues</div>
+    <div class="panel-body">
+        <div class="panel panel-default">
+            <div class="panel-body issue 1">
+                <h4>Form validation errors</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 2">
+                <h4>Unfinished checkout rate</h4>
+                <div class="issue-actions">
+                    <a class="error" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 3">
+                <h4>Search efficiency</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 4">
+                <h4>Memorability</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 5">
+                <h4>Support satisfaction</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 6">
+                <h4>Filling forms</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 7">
+                <h4>Copyrighting</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+            <div class="panel-body issue 8">
+                <h4>Navigation</h4>
+                <div class="issue-actions">
+                    <a class="add-to-list" data-toggle="tooltip" data-placement="top" data-original-title="Add to list" href="#"><i class="fa fa-th-list"></i></a>
+                    <a class="issue-details" data-toggle="tooltip" data-placement="top" data-original-title="Details" href="#"><i class="fa fa-info-circle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="view-all-issues"><a href="#">View all issues<i class="fa fa-info-circle"></a></i></div>
+    </div>
+</div>
+</div>
